1434535974c6bfd37229a25e6a1db532512c9f4d
queise <jordicasa@gmail.com>
2014-08-27 23:53:33 +0200

    Create wm_main_onlyplot.py

---
+++ wm_main_onlyplot.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+from wm_plot import *
+
+def main():
+
+# Lists of journals, areas  and dates to be analysed:
+  journals = [ 'nyt', 'lat', 'bbc', 'elp', 'chd', 'toi' ]
+  areas    = [ 'USA' ,'USA', 'Europe', 'Europe', 'Asia', 'Asia' ]
+
+  df = pd.DataFrame( {u'date' : ['2014-08-18', '2014-08-19', '2014-08-20', '2014-08-21', '2014-08-22',
+                                 '2014-08-18', '2014-08-19', '2014-08-20', '2014-08-21', '2014-08-22',
+                                 '2014-08-18', '2014-08-19', '2014-08-20', '2014-08-21', '2014-08-22',
+                                 '2014-08-18', '2014-08-19', '2014-08-20', '2014-08-21', '2014-08-22',
+                                 '2014-08-18', '2014-08-19', '2014-08-20', '2014-08-21', '2014-08-22',
+                                 '2014-08-18', '2014-08-19', '2014-08-20', '2014-08-21', '2014-08-22'],
+                      u'journal': ['nyt', 'nyt', 'nyt', 'nyt', 'nyt',
+                                   'lat', 'lat', 'lat', 'lat', 'lat',
+                                   'bbc', 'bbc', 'bbc', 'bbc', 'bbc', 
+                                   'elp', 'elp', 'elp', 'elp', 'elp', 
+                                   'chd', 'chd', 'chd', 'chd', 'chd', 
+                                   'toi', 'toi', 'toi', 'toi', 'toi'],
+                      u'area': ['USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA', 'USA',
+                                'Europe', 'Europe', 'Europe', 'Europe', 'Europe', 'Europe', 'Europe', 'Europe', 'Europe', 'Europe',
+                                'Asia', 'Asia', 'Asia', 'Asia', 'Asia', 'Asia', 'Asia', 'Asia', 'Asia', 'Asia'],
+                      u'score': [-0.36363636, -0.225, -0.48979592, -0.02439024, -0.91489362,
+                                 -0.3,  -0.48837209, -0.26666667, 0.15151515, -0.18918919,
+                                 0.06666667, 0.24137931, -0.41025641, -0.35483871, -0.71428571,
+                                 0.16666667, -0.88571429, -1.15625, -0.81481481, -0.45,
+                                 0.83333333, 0.95348837, 0.87234043, 0.86, 0.52777778,
+                                 -0.03225806, -1.05882353, -0.23076923, -0.88888889, -0.6] })
+
+  df.date = pd.to_datetime(df.date,format='%Y-%m-%d')
+
+  # plots
+  F_plot('wm.html',df,journals,areas)
+
+if __name__ == '__main__':
+  main()
